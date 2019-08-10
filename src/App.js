7d4ed7be61ce04692a97b32f022ae0b0c24ea2fa
Samir Standnes Abdi <samir@Samirs-MacBook-Air.local>
2019-08-10 22:04:40 +0200

    css on ContentSections box-shadow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
+
+// Import material design for navbar
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+
 import "./App.css";
+
 
 import NavBar from "./Components/NavBar/NavBar";
 import ContentSection from "./Components/ContentSection/ContentSection";
@@ -18,9 +25,13 @@ class App extends React.Component {
     return (
       <div className="App">
         <NavBar />
+        <div className='content_sections'>
         <ContentSection id_name="blue" text={this.state.concept} />
         <ContentSection id_name="white" text="Text Section 2" />
         <ContentSection id_name="blue" text="Text Section 3" />
+
+        </div>
+        <div className="shadow lifted"> hello</div>
       </div>
     );
   }
--- src/Components/ContentSection/ContentSection.js
+++ src/Components/ContentSection/ContentSection.js
@@ -1,20 +1,23 @@
 import React from "react";
+import './ContentSection.css';
 
 const ContentSection = (props): String => {
 	return (
 		// Need to add in a id equal to href in nav bar.
-		<div id={props.id_name} className='ContentSectionStructure'>
+		<div id={props.id_name} className="ContentSectionStructure">
 			{/* h2 element need to get the name of the sectioon passed to it */}
-			<h2> Name of section </h2>
+			<div >
+				<h2> Name of section </h2>
 
-			{/* text are need to get text data of the section passed to it */}
-			<div>
-
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt necessitatibus obcaecati quos a similique quam quisquam voluptate reiciendis nesciunt, ducimus blanditiis pariatur expedita iusto, officiis odit architecto beatae accusamus corporis!
-
+				{/* text are need to get text data of the section passed to it */}
+				<div>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					Incidunt necessitatibus obcaecati quos a similique quam
+					quisquam voluptate reiciendis nesciunt, ducimus blanditiis
+					pariatur expedita iusto, officiis odit architecto beatae
+					accusamus corporis!
+				</div>
 			</div>
-
-			
 		</div>
 	);
 };
