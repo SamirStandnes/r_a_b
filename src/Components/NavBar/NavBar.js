3897a546e70d9be1ef8c12efe43d3091ff5abf8e
Samir Standnes <samir.standnes@lanekassen.no>
2019-08-29 15:12:16 +0200

    nav bar functionality

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,25 +3,38 @@ import logo from "../../logo.svg";
 import "../../App.css";
 
 class NavBar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			'menu_toggled': false
-		};
-	}
-	render() {
-		return (
-			<nav>
-				<img id="logo" src={logo} />
-				<ul id="navbar_ul_items">
-					<li target="_blank"> Concept </li>
-					<li target="_blank"> Confidenciality </li>
-					<li target="_blank"> Contact </li>
-					<li target="_blank"> Prices </li>
-				</ul>
-			</nav>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu_toggled: true,
+      menu_closed: "fas fa-bars",
+      menu_open: "fas fa-times"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      menu_toggled: !this.state.menu_toggled
+    });
+    console.log("pressed");
+  }
+  render() {
+    return (
+      <nav style={{backgroundColor: 'yellow'}}>
+        <img id="logo" src={logo} />
+		<div className={this.state.menu_toggled ? "menu_hide" : "menu_show"}>
+          <ul id="navbar_ul_items">
+            <li target="_blank"> Concept </li>
+            <li target="_blank"> Confidenciality </li>
+            <li target="_blank"> Contact </li>
+            <li target="_blank"> Prices </li>
+          </ul>
+        </div>
+		<i id="menu_icon" onClick={this.handleClick} className={this.state.menu_toggled ? this.state.menu_closed: this.state.menu_open}></i>
+		
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
