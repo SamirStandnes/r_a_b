e85aa409c85dbdab9b2cb9368ae0ef93e4fc317d
Samir Standnes Abdi <samir@sams-air.lan>
2019-12-21 00:05:41 +0100

    work in font, numbers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
-
+import data from "./Data";
 import "./App.css";
 
 import NavBar from "./Components/NavBar/NavBar";
 import ContentSection from "./Components/ContentSection/ContentSection";
 
+console.log(data);
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      concept: "Text Section 1"
-    };
+    this.state = data;
   }
-
+ 
   render() {
     return (
       <div className="App">
         <img id="logo" src={logo} />
         <NavBar />
         <div className="content_sections">
-          <ContentSection id_name="blue" text={this.state.concept} />
-          <ContentSection id_name="white" text="Text Section 2" />
+          <ContentSection id_name="blue" text={this.state.Concept} />
+          <ContentSection id_name="white" text="'Text" />
           <ContentSection id_name="blue" text="Text Section 3" />
         </div>
       </div>
