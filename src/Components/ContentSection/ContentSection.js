bb2fe2d13831468db2b2bfd280a92c3b443bcb47
Samir Standnes Abdi <samir@sams-air.lan>
2019-12-22 14:57:54 +0100

    finish navbar logic and dataflow

--- src/Components/ContentSection/ContentSection.js
+++ src/Components/ContentSection/ContentSection.js
@@ -7,7 +7,7 @@ const ContentSection = (props) => {
 		<div id={props.id_name} className="ContentSectionStructure">
 			{/* h2 element need to get the name of the sectioon passed to it */}
 			<div >
-				<h2> Name of section </h2>
+				<h2> {props.id_name}</h2>
 
 				{/* text are need to get text data of the section passed to it */}
 				<div>
