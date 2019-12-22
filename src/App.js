6886ea5f5e0394b0380a5112d6573d104639bdc9
Samir Standnes Abdi <samir@sams-air.lan>
2019-12-22 19:46:29 +0100

    data logic etc...

--- src/App.js
+++ src/App.js
@@ -19,20 +19,54 @@ function obj_to_array(obj) {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = obj_to_array(data);
+    this.state = {
+      data: obj_to_array(data),
+      headings: ["Concept", "Confidentiality", "Prices", "Contact"]
+    };
   }
 
   render() {
     return (
       <div className="App">
         <img id="logo" src={logo} />
-        <NavBar headings={this.state.map((element) => element[0])} />
+        <NavBar headings={this.state.headings.map(element => element)} />
         <div className="content_sections">
-          {
-            this.state.map(element => (
-               <ContentSection id={element[0]} id_name={element[0]} text={element[1]} />
-            ))
-          }
+          {this.state.data.map(element => (
+            <ContentSection
+              id={element[0]}
+              id_name={element[0]}
+              text={element[1]}
+            />
+          ))}
+          <div id="Prices" className="ContentSectionStructure">
+            <div>
+              <h2> Prices </h2>
+              <div>Prices Price</div>
+            </div>
+          </div>
+
+          <div id="Contact" className="ContentSectionStructure">
+            <div>
+              <h2> Contact </h2>
+              <div>
+                The starting point for every customer relationship is always
+                <span id="contact_email">
+                  {" "}
+                  <a href="mailto: contact@rent-a-brain.community?">
+                    contact@rent-a-brain.community{" "}
+                  </a>
+                </span>
+                <br />
+                <br />
+                If you are interested in the “Personal - Encrypted” product
+                please use what is colloquially called a “burner email account”
+                when reaching out. The email should be sent with a subject line
+                of “Interested in PE” and nothing else, we will then reply with
+                further communication instructions. If you do not know how to
+                set up a “burner email account”, read this simple guide.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/Data.js
+++ src/Data.js
@@ -1,8 +1,6 @@
 const DATA = {
-	'Concept': 'Testing 100',
-	'Confidentiality': 'Testing2',
-	'Contact': 'Testing3',
-	'Prices': 'Testing4'
+	Concept:"Rent A Brain® allows anyone on-demand access to high capacity human computation and action, with 100% confidentiality. Anyone faced with a problem can access Rent A Brain’s services, be it a theoretical, hypothetical, ethical or a practical problem, on-demand. If you or your business needs on-demand high capacity problem-solving and/or technical capabilities, Rent A Brain is here without judgment and 100% confidentiality. ",
+	Confidentiality:"Rent A Brain allows anyone to solicit our advice in 100% confidentiality,  always. Unparalleled integrity is the hallmark of our business. Accordingly, we provide the option of complete data anonymity in all aspects of our customer relations, including communication and payment options. You can use our services and no one will ever have to know, not even Rent A Brain. ",
 };
 
 export default DATA;
